fad5429817b59887c8217fbb16ce3b71ac6e8cea
MaximWuyts <32163906+MaximWuyts@users.noreply.github.com>
2017-10-06 10:21:47 +0200

    commit 1st assignment

---
+++ opdracht-comments.php
@@ -0,0 +1,25 @@
+<?php 
+
+$voornaam   =   'Maxim';
+$achternaam =   'Wuyts';
+
+/*  Maxim Wuyts
+    maxim.wuyts@gmail.com 
+*/
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Opdracht comments </title>
+</head>
+<body>
+    <h1>Opdracht comments</h1>
+    <p>  <?php echo $voornaam . ' ' . $achternaam  ?>   </p>
+</body>
+</html>
